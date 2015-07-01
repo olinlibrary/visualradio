c8fa09b3d1828d94e2cb4f0fbffca46ea79dee37
Nick Eyre <nick@nickeyre.com>
2015-07-01 14:28:58 -0400

    Prevent Access to Private Folders

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 
 $f3=require('lib-fatfree/base.php');
+$f3->config('config.cfg');
 require('video.php');
 require('channel.php');
 
 $f3->set('DEBUG',1);
-$f3->set('db',new DB\SQL('sqlite:db/visualradio.db'));
+$f3->set('db',new DB\SQL('sqlite:'.$f3->get('databaseFile')));
 
 $f3->route('GET /', function($f3) {
 	// Get Channels
@@ -14,18 +15,20 @@ $f3->route('GET /', function($f3) {
 
 	echo Template::instance()->render('templates/index.html');
 });
-
-$f3->route('GET @channelList: /channels', 'Channel->viewAll');
-
-$f3->route('GET /channel/new', 'Channel->addForm');
-$f3->route('POST /channel/new', 'Channel->add');
-$f3->route('GET @channelEdit: /channel/@channelID/edit', 'Channel->editForm');
-$f3->route('POST /channel/@channelID/edit', 'Channel->edit');
-
 $f3->route('GET /channel/@channelID/status', 'Channel->status');
-$f3->route('GET /channel/@channelID/new', 'Video->addForm');
-$f3->route('POST /channel/@channelID/new', 'Video->add');
-$f3->route('GET /channel/@channelID/@videoID', 'Video->editForm');
-$f3->route('POST /channel/@channelID/@videoID', 'Video->edit');
+
+if($f3->get('enableAdmin')){
+
+	$f3->route('GET @channelList: /channels', 'Channel->viewAll');
+	$f3->route('GET /channel/new', 'Channel->addForm');
+	$f3->route('POST /channel/new', 'Channel->add');
+	$f3->route('GET @channelEdit: /channel/@channelID/edit', 'Channel->editForm');
+	$f3->route('POST /channel/@channelID/edit', 'Channel->edit');
+	$f3->route('GET /channel/@channelID/new', 'Video->addForm');
+	$f3->route('POST /channel/@channelID/new', 'Video->add');
+	$f3->route('GET /channel/@channelID/@videoID', 'Video->editForm');
+	$f3->route('POST /channel/@channelID/@videoID', 'Video->edit');
+}
+
 
 $f3->run();
